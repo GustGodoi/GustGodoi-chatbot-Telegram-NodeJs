498d3d3283353ce5e0f39a279492a398ab8c0d81
Gustavo Godoi <gustavogodoi442@gmail.com>
2020-10-28 20:16:21 -0300

    Trocar forEach por for of para async await

--- bot_04.js
+++ bot_04.js
@@ -35,12 +35,14 @@ bot.on('voice', ctx => {
 })
 
 //Evento de foto
-bot.on('photo', ctx => {
+bot.on('photo', async ctx => {
     const photo = ctx.message.photo
+    const legenda = ctx.message.caption
     console.log(photo)
-    photo.forEach((ft, i) => {
-        ctx.reply(`Foto ${i} tem resolução de ${ft.width}x${ft.height}`)
-    })
+    for([i, ft] of photo.entries()){
+        await ctx.reply(`Foto ${i} tem resolução de ${ft.width}x${ft.height}`)
+    }
+    ctx.reply(`Legenda: ${legenda}`)
 })
 
 //Evento de figurinhas
